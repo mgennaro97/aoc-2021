770ed31ead31a8a60b2704fa0fbfcb3b606d254c
Mattia Gennaro <mattia.gennaro@dmwgroup.co.uk>
2021-12-04 12:56:06 +0000

    God bless numpy

---
+++ day4/day4-2.py
@@ -0,0 +1,40 @@
+import datetime
+import numpy as np
+
+given_input = []
+bingo_boards = []
+
+start_time = datetime.datetime.now()
+
+
+def solve():
+    bingo_reshaped = []
+    for board in bingo_boards:
+        bb = np.array(board)
+        reshaped = bb.reshape(5, 5)
+        bingo_reshaped.append(reshaped)
+
+    for num in given_input:
+        for reshaped in bingo_reshaped:
+            result = np.where(reshaped == num)
+            if len(result[0]) > 0 and len(result[1]) > 0:
+                reshaped[int(result[0])][int(result[1])] = -1
+            else:
+                pass
+            if any(np.equal(reshaped, [-1, -1, -1, -1, -1]).all(1)) \
+                    or any(np.equal(reshaped, [-1, -1, -1, -1, -1]).all(0)):
+                if len(bingo_reshaped) > 1:
+                    bingo_reshaped = [a for a, skip in
+                      zip(bingo_reshaped,
+                          [np.allclose(a, reshaped) for a in bingo_reshaped]) if
+                      not skip]
+                else:
+                    print(sum(reshaped[reshaped != -1]) * num)
+                    return
+
+
+end_time = datetime.datetime.now()
+print("μs: " + str((end_time - start_time).microseconds))
+
+if __name__ == '__main__':
+    solve()
